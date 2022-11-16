33053b883c5f8a7a14a080e8f0918d4e6e5199bf
laurafernandezk <laurafernandezk@gmail.com>
2022-11-16 12:02:28 -0300

    segundo desafio

--- src/styles.js
+++ src/styles.js
@@ -8,40 +8,6 @@ export const styles = StyleSheet.create({
     messageContainer:{
       alignItems: "center",
       marginTop: 20
-    },
-    modalContainer:{
-      justifyContent:"center",
-      alignItems:"center",
-      marginTop:70,
-      marginHorizontal:20,
-      
-      padding: 20,
-      backgroundColor:"white",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 3,
-      },
-      shadowOpacity: 0.29,
-      shadowRadius: 4.65,
-  
-      elevation: 7,
-    },
-    modalTitle:{
-      marginBottom:10,
-      fontSize: 18,
-      fontWeight:"bold"
-    },
-    modalDetailContainer:{
-      paddingVertical: 20,
-    },
-    modalDetailText:{
-      fontSize: 14,
-  
-    },
-    modalButton:{
-      flexDirection:"row",
-      justifyContent:"space-between",
-      width:"60%"
     }
+    
   })
